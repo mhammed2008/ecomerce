2108105f24a5be223863dbf3f845b85186a079cc
mhammed2008 <79751749+mhammed2008@users.noreply.github.com>
2025-03-03 23:14:30 +0200

    hello

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,6 +3,7 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { withHashLocation } from '@angular/router';
 
 const serverConfig: ApplicationConfig = {
   providers: [
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {provideRouter, withInMemoryScrolling} from '@angular/router';
+import {provideRouter, withHashLocation, withInMemoryScrolling} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -15,7 +15,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideHttpClient(withFetch(),withInterceptors([authInterceptor])),
-    provideRouter(routes , withInMemoryScrolling({scrollPositionRestoration: 'top'})),
+    provideRouter(routes , withInMemoryScrolling({scrollPositionRestoration: 'top'}),withHashLocation()),
     provideAnimations(),
     provideClientHydration(withEventReplay()),
     provideToastr(),
